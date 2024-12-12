1c8a86fd5a0cdbc3d5211b66b5bba969168d0cd5
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-12-12 01:29:45 -0600

    add/remove books from shelf, list/search for book titles

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,25 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(title, shelf) {
+  const bookIndex = shelf.findIndex(book => book.title === title);
+  return shelf.splice(bookIndex, 1);
+}
+
+function listTitles (shelf) {
+  return shelf.map(book => book.title).join(", ");
+}
+
+function searchShelf(shelf, title) {
+  return shelf.some(book => book.title === title);
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
